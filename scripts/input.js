3b2fa8af2ad030e360755bb17c1031832197b772
Ttae27 <ponghong.2744@gmail.com>
2025-03-26 00:02:17 +0700

    encrypt decrypt

--- scripts/input.js
+++ scripts/input.js
@@ -1,6 +1,15 @@
 const crypto = require('crypto');
 var prompt = require('prompt-sync')();
 
+function base64ToArrayBuffer(base64) {
+    var binaryString = atob(base64);
+    var bytes = new Uint8Array(binaryString.length);
+    for (var i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
 const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
     modulusLength: 512,
     publicKeyEncoding: {
@@ -20,13 +29,20 @@ console.log('Private Key:', privateKey);
 
 //bidder1
 const bidder1 = prompt('bid_1 : ');
+const enc_bid1 = crypto.publicEncrypt(publicKey, Buffer.from(bidder1)).toString('base64');
+console.log(enc_bid1)
 
 //bidder2
 const bidder2 = prompt('bid_2 : ');
+const enc_bid2 = crypto.publicEncrypt(publicKey, Buffer.from(bidder1)).toString('base64');
+console.log(enc_bid2)
 
 //bidder3
 const bidder3 = prompt('bid_3 : ');
+const enc_bid3 = crypto.publicEncrypt(publicKey, Buffer.from(bidder1)).toString('base64');
+console.log(enc_bid3)
 
 //end auction
-
-
+const winner = base64ToArrayBuffer(enc_bid1)
+const dec_bid1 = crypto.privateDecrypt(privateKey, winner).toString();
+console.log(dec_bid1)
