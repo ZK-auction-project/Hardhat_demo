842b4d8757800d2437369fa0e9987d19250d7772
Ttae27 <ponghong.2744@gmail.com>
2025-03-26 17:38:09 +0700

    eiei

--- scripts/generate_proof_range.mjs
+++ scripts/generate_proof_range.mjs
@@ -5,33 +5,31 @@ import path from "path"
 import fse from 'fs-extra'
 import promptSync from 'prompt-sync';
 
-const prompt = promptSync();
 export async function proof_range(input_1, input_2) {
-initialize().then(async (zokratesProvider) => {
+    initialize().then(async (zokratesProvider) => {
 
-    let rawdata = fs.readFileSync('./zokrates/range.zok');
+        let rawdata = fs.readFileSync('./zokrates/range.zok');
 
-    const source = rawdata.toString();
+        const source = rawdata.toString();
 
-    const artifacts = zokratesProvider.compile(source);
+        const artifacts = zokratesProvider.compile(source);
 
-    // const input_1 = prompt('input1 :');
+        const { witness, output } = zokratesProvider.computeWitness(artifacts, [input_1, input_2]);
 
-    // const input_2 = prompt('input2 :');
+        const keypair = zokratesProvider.setup(artifacts.program);
 
-    const { witness, output } = zokratesProvider.computeWitness(artifacts, [input_1, input_2]);
+        const proof = zokratesProvider.generateProof(artifacts.program, witness, keypair.pk);
+        const result = new Array(proof.proof.a, proof.proof.b, proof.proof.c);
+        fse.outputFile("./proofs/proof_range.json", JSON.stringify(proof));
+        console.log(chalk.green("\nProofs generated successfully"));
+        return result;
+        // const verifier = zokratesProvider.exportSolidityVerifier(keypair.vk, "v1");
+        // fse.outputFile("./contracts/verifier_test.sol", verifier);
+        // console.log(chalk.green("\nContracts generated successfully"));
 
-    const keypair = zokratesProvider.setup(artifacts.program);
-
-    const proof = zokratesProvider.generateProof(artifacts.program, witness, keypair.pk);
-    fse.outputFile("./proofs/proof_range.json", JSON.stringify(proof));
-    console.log(chalk.green("\nProofs generated successfully"));
-
-    // const verifier = zokratesProvider.exportSolidityVerifier(keypair.vk, "v1");
-    // fse.outputFile("./contracts/verifier_test.sol", verifier);
-    // console.log(chalk.green("\nContracts generated successfully"));
-
-});}
+    });
+    
+}
 
 // import { initialize } from 'zokrates-js';
 // import chalk from 'chalk';
--- scripts/input.js
+++ scripts/input.js
@@ -1,8 +1,12 @@
-import { proof_range } from "file:///C:/Users/Mon/Desktop/Hardhat_demo/scripts/generate_proof_range.mjs";
-import { proof_compare } from "file:///C:/Users/Mon/Desktop/Hardhat_demo/scripts/generate_proof_compare.mjs";
+import { proof_range } from "./generate_proof_range.mjs";
+import { proof_compare } from "./generate_proof_compare.mjs";
 import crypto from 'crypto';
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
+
+const range = proof_range("2","0")
+console.log(range)
+// const compare = proof_compare("5", "6", "7", ["263561599766550617289250058199814760685", "65303172752238645975888084098459749904"], ["296016139321527823785053958024045515449", "169585634993304848991863197817116667302"], ["62133134181886812829768166950054220896", "160635334427203623512968684759912538624"])
 
 function base64ToArrayBuffer(base64) {
     var binaryString = atob(base64);
@@ -35,10 +39,6 @@ const bidder1 = prompt('bid_1 : ');
 const enc_bid1 = crypto.publicEncrypt(publicKey, Buffer.from(bidder1)).toString('base64');
 console.log(enc_bid1)
 
-const range = proof_range("2","0")
-const compare = proof_compare("5", "6", "7", ["263561599766550617289250058199814760685", "65303172752238645975888084098459749904"], ["296016139321527823785053958024045515449", "169585634993304848991863197817116667302"], ["62133134181886812829768166950054220896", "160635334427203623512968684759912538624"])
-
-// console.log(proof_range(2, 0));
 //bidder2
 const bidder2 = prompt('bid_2 : ');
 const enc_bid2 = crypto.publicEncrypt(publicKey, Buffer.from(bidder2)).toString('base64');
